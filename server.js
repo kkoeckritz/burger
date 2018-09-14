20a5682e33f313c76eef1df31e6132de9fa5f855
Kris Koeckritz <kjk464@humboldt.edu>
2018-09-13 19:01:01 -0700

    add initial db files

---
+++ server.js
@@ -0,0 +1,21 @@
+// include required libs
+var path = require("path");
+var fileType = require("file-type");
+var parser = require("body-parser");
+var express = require("express");
+
+// configure, initialize server
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, "app/public")));
+app.use(parser.urlencoded({extended: true}));
+app.use(parser.json());
+
+// link server routing data
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app, path);
+
+// start server
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+});
